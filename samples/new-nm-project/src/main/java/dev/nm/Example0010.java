5a134d6af88faa3849d852124c3bef839c0c9892
Haksun Li <haksun.li@nm.dev>
2022-10-28 19:50:15 +1100

    added a sample project
    made sure it compiles and runs

---
+++ samples/new-nm-project/src/main/java/dev/nm/Example0010.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) NM LTD.
+ * https://www.nm.dev/
+ * 
+ * THIS SOFTWARE IS LICENSED, NOT SOLD.
+ * 
+ * YOU MAY USE THIS SOFTWARE ONLY AS DESCRIBED IN THE LICENSE.
+ * IF YOU ARE NOT AWARE OF AND/OR DO NOT AGREE TO THE TERMS OF THE LICENSE,
+ * DO NOT USE THIS SOFTWARE.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITH NO WARRANTY WHATSOEVER,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION,
+ * ANY WARRANTIES OF ACCURACY, ACCESSIBILITY, COMPLETENESS,
+ * FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, NON-INFRINGEMENT, 
+ * TITLE AND USEFULNESS.
+ * 
+ * IN NO EVENT AND UNDER NO LEGAL THEORY,
+ * WHETHER IN ACTION, CONTRACT, NEGLIGENCE, TORT, OR OTHERWISE,
+ * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIMS, DAMAGES OR OTHER LIABILITIES,
+ * ARISING AS A RESULT OF USING OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package dev.nm;
+
+/**
+ *
+ * @author haksunli
+ */
+public class Example0010 {
+
+    public static void main(String[] args) {
+        Example0010 example0010 = new Example0010();
+        example0010.function1();
+    }
+
+    public void function1() {
+        System.out.println("Hello World!");
+    }
+
+    public int function2() {
+        return 2;
+    }
+
+}
