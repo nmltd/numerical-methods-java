2b4781daff39e714e78e1dbfebf3ae59c9e62adb
Mansi <107349957+Mannxxx@users.noreply.github.com>
2022-11-09 16:49:31 +0530

    Update ColtImplementation.java

--- benchmark/src/main/java/com/numericalmethod/benchmark/implementation/ColtImplementation.java
+++ benchmark/src/main/java/com/numericalmethod/benchmark/implementation/ColtImplementation.java
@@ -126,7 +126,11 @@ public class ColtImplementation extends AbstractImplementation {
             @Override
             public void execute(Object[] arguments) {
                 DoubleMatrix2D B = matrix(arguments[0]).copy();
+<<<<<<< Updated upstream
                 DoubleFunction my_obj = new my_class(MatrixScale.SCALAR);
+=======
+                my_class my_obj = new my_class(MatrixScale.SCALAR);
+>>>>>>> Stashed changes
                 B.assign(my_obj);
                 // B.assign(Functions.abs {
                 //     @Override
